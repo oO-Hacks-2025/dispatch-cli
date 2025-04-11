2009d379efeaa847810a71b47d8a8ead90d03e2b
Gabriel Mureșan <32938828+muresan-gabriel@users.noreply.github.com>
2025-04-11 18:44:49 +0300

    FEAT: New & Improved Logging (#1)
    * feat: new & improved logging
    
    * chore: revert dumb gpt change of agent

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿using System.Net.Http.Headers;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+    })
+    .Build();
+
+ILogger<Program> logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Starting the HTTP client setup...");
 
 using HttpClient client = new();
 client.DefaultRequestHeaders.Accept.Clear();
 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 client.DefaultRequestHeaders.Add("User-Agent", "[object Object] agent");
 
+logger.LogInformation("HTTP client configured.");
