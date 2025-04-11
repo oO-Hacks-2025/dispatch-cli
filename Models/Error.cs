f2bfb82f331415d28ec6e40d130c6073be7a5179
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2025-04-11 23:18:17 +0300

    Added Comments

--- Models/Error.cs
+++ Models/Error.cs
@@ -1,12 +1,22 @@
 using System.Text.Json.Serialization;
 
 namespace testing.Models;
-
+/// <summary>
+///  Error represents the number of ambulances that were missed or over dispatched.
+/// </summary>
+/// <param name="missed"></param>
+/// <param name="overDispatched"></param>
 public class Error(int missed, int overDispatched)
 {
+    /// <summary>
+    /// The number of ambulances that were missed.
+    /// </summary>
     [JsonPropertyName("missed")]
     public int Missed { get; set; } = missed;
 
+    /// <summary>
+    /// The number of ambulances that were over dispatched.
+    /// </summary>
     [JsonPropertyName("overDispatched")]
     public int OverDispatched { get; set; } = overDispatched;
 }
