e1292d5d227cb476c11989dc83d0a6d4552a81b5
Catalin Iuga <cataliniuga34@gmail.com>
2025-04-11 18:17:10 +0300

    chore: init

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Add("User-Agent", "[object Object] agent");
+
